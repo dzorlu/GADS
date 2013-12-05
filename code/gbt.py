824aca59b56188a5fb8be4dd15c9cba2deb7a113
dzorlu <dzorlu@sumall.org>
2013-12-04 20:51:22 -0500

    updated gbt.py

--- code/gbt.py
+++ code/gbt.py
@@ -23,7 +23,7 @@ def main():
     print 'decision stump acc = {}\t(max_depth = 1)'.format(stump_score)
 
     # single dec tree (max_depth=3)
-    tree_clf = DecisionTreeClassifier(max_depth=5)
+    tree_clf = DecisionTreeClassifier(max_depth=3)
     tree_clf.fit(X_train, y_train)
     tree_score = round(tree_clf.score(X_test, y_test), 3)
     print 'decision tree acc = {}\t(max_depth = 5)\n'.format(tree_score)
@@ -41,7 +41,23 @@ def main():
         gbt_clf.fit(X_train, y_train)
         gbt_scores.append(round(gbt_clf.score(X_test, y_test), 3))
 
-    print '\ngbt accuracy =\n{}'.format(gbt_scores)
+    print '\ngbt accuracy =\n{}\n'.format(gbt_scores)
+
+    # stochastic gbt (using subsampling)
+    sgbt_scores = list()
+    for k in (10, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500):
+        print 'fitting sgbt for n_estimators = {}...'.format(k)
+
+        sgbt_clf = GradientBoostingClassifier(
+            n_estimators=k,         # number of weak learners for this iteration
+            max_depth=1,            # weak learners are dec stumps
+            subsample=0.5,          # % of training set used by each bc
+            learning_rate=1.0)      # regularization (shrinkage) hyperparam
+
+        sgbt_clf.fit(X_train, y_train)
+        sgbt_scores.append(round(sgbt_clf.score(X_test, y_test), 3))
+
+    print '\nsgbt accuracy =\n{}'.format(sgbt_scores)
 
 if __name__ == '__main__':
     main()
