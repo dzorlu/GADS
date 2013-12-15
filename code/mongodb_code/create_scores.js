a2962ea98f8f6ec5d61580ce6a7b0c9ad9a9e4a2
dzorlu <dzorlu@sumall.org>
2013-12-15 15:55:44 -0500

    Mongo indexing example

---
+++ code/mongodb_code/create_scores.js
@@ -0,0 +1,13 @@
+use school
+db.students.drop();
+types = ['exam', 'quiz', 'homework', 'homework'];
+for (i = 0; i < 1000000; i++) {
+    scores = []
+    for (j = 0; j < 4; j++) {
+	scores.push({'type':types[j],'score':Math.random()*100});
+    }
+    record = {'student_id':i, 'scores':scores};
+    db.students.insert(record);
+
+}
+	    
