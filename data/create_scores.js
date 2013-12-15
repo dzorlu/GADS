a1ebbafca390f6fc2432f149089ff53a9a3774ee
dzorlu <dzorlu@sumall.org>
2013-12-15 15:58:17 -0500

    mongo indexing data

--- data/create_scores.js
+++ data/create_scores.js
@@ -1,7 +1,7 @@
 use school
 db.students.drop();
 types = ['exam', 'quiz', 'homework', 'homework'];
-for (i = 0; i < 1000000; i++) {
+for (i = 0; i < 10000000; i++) {
     scores = []
     for (j = 0; j < 4; j++) {
 	scores.push({'type':types[j],'score':Math.random()*100});
