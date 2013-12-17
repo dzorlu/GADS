2d4f734060dbd8c40909b59eb2b139480a6a509d
dzorlu <dzorlu@sumall.org>
2013-12-17 09:46:52 -0500

    MongodB Aggregation Datasets

---
+++ code/simple_aggregation.js
@@ -0,0 +1,23 @@
+use agg
+
+
+db.products.aggregate([
+    {$group:
+     {
+	 _id:"$manufacturer", 
+	 num_products:{$sum:1}
+     }
+    }
+])
+
+
+db.products.aggregate([
+    {$group:
+     {
+	 _id: {'manufacturer':"$manufacturer"},
+	 num_products:{$sum:1}
+     }
+    }
+])
+
+
